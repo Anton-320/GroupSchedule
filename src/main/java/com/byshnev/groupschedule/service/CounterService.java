96edbd017aeabf5bae4e371aba5b2335200e6b20
Anton-320 <abyshnevaking@mail.ru>
2024-05-02 19:17:49 +0300

    lab 4 (вроде надеюсь готово) commit

--- src/main/java/com/byshnev/groupschedule/service/CounterService.java
+++ src/main/java/com/byshnev/groupschedule/service/CounterService.java
@@ -1,16 +1,16 @@
 package com.byshnev.groupschedule.service;
 
 
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
+@NoArgsConstructor
 public class CounterService {
 
   private static final AtomicInteger count = new AtomicInteger(0);
-
-  public CounterService() { }
 
   public static synchronized int increment() {
     return count.incrementAndGet();
